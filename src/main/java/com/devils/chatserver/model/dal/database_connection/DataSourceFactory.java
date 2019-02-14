cb596a47e40835b523c5a7c2545518c5bcb40ecd
Islam El-Rougy <Islam El-Rougy@DESKTOP-T6U6IRA>
2019-02-14 18:37:00 +0200

    initial commit

---
+++ src/main/java/com/devils/chatserver/model/dal/database_connection/DataSourceFactory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devils.chatserver.model.dal.database_connection;
+
+/**
+ *
+ * @author Islam El-Rougy
+ */
+
+/*Get Data Source object using Singleton*/
+public class DataSourceFactory {
+    
+}
